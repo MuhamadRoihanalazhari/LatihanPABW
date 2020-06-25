d4285098a0c2bcbdb0f672cd1732483d854c2b27
Muhamad Roihan alAzhari <58973520+MuhamadRoihanalazhari@users.noreply.github.com>
2020-06-25 23:08:41 +0700

    Add files via upload

---
+++ php/include/Config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * RetrofitExample
+ * https://github.com/quintuslabs/RetrofitExample
+ * Created on 10-june-2018.
+ * Created by : Santosh Kumar Dash:- http://santoshdash.epizy.com
+ */
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'retrofit');
+
+define('USER_CREATED_SUCCESSFULLY', 0);
+define('USER_CREATE_FAILED', 1);
+define('USER_ALREADY_EXISTED', 2);
+
+?>
