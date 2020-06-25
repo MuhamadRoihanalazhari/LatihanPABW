060a15b33e2c25372799273fdfa26c886df65781
Muhamad Roihan alAzhari <58973520+MuhamadRoihanalazhari@users.noreply.github.com>
2020-06-26 00:08:22 +0700

    Update proses-masuk.php

--- Login/proses-masuk.php
+++ Login/proses-masuk.php
@@ -9,12 +9,12 @@ $email = $_POST['email'];
 $query_login = mysqli_query($db, "SELECT * FROM user WHERE email='$email' && password='$password'");
 
 if (mysqli_num_rows($query_login) == 0) {
-    header("location:https://roihanpabw.000webhostapp.com/index.php?notif=true");
+    header("location:http://kabupatenriau.tif18e.com/index.php?notif=true");
 } else {
     $row_login = mysqli_fetch_assoc($query_login);
     session_start();
     $_SESSION['id_user'] = $row_login['id_user'];
     
 
-    header("location:https://roihanpabw.000webhostapp.com/index1.php");
+    header("location:http://kabupatenriau.tif18e.com/index1.php");
 }
